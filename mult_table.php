fb074a85f7f702bcab95ba2b153fd21985f35d8b
RobDog04 <78656529+RobDog04@users.noreply.github.com>
2021-04-27 22:57:39 -0500

    Update mult_table.php

--- mult_table.php
+++ mult_table.php
@@ -1,12 +1,16 @@
-<?php
-for ($i = 1; $i < 101; $i++) {
-  for ($j = 1; $j < 101; $j++) {
-     if ($j == 1) {
-       echo str_pad($i*$j, 2, " ", STR_PAD_LEFT);
-     } else {
-       echo str_pad($i*$j, 4, " ", STR_PAD_LEFT);
-     }
-  }
-  echo "<br>";
-}
+<?php 
+for( $i = 1; $i < 101; $i++)
+{
+echo "<tr>";
+    for( $j=1; $j<101; $j++)
+    {
+    "<td>";
+    echo( $i*$j);
+    echo("  ");
+      "</td>" ;
+   } 
+echo "<br>";          
+echo  "</tr>";
+
+}        
 ?>
