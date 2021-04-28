6d407c9cbbae9f01406f8216db37f1685689d95d
RobDog04 <78656529+RobDog04@users.noreply.github.com>
2021-04-27 22:18:38 -0500

    Add files via upload

---
+++ Quiz.php
@@ -0,0 +1,27 @@
+<?php
+$counter = 0;
+if (answers = true) 
+  if(ans6 = true)
+    echo(" You answered 6," <br> "The correct answer was 6"<br><br>);
+    counter+1;
+  if(ans5 = true)
+    echo(" You answered 5," <br> "The correct answer was 5"<br><br>);
+    counter+1;
+  if(ans8 = true)
+    echo(" You answered 8," <br> "The correct answer was 8"<br><br>);
+    counter+1;
+  if(ans18 = true)
+    echo(" You answered 18," <br> "The correct answer was 18"<br><br>);
+    counter+1;
+  if(ansBlue = true)
+    echo(" You answered Blue," <br> "The correct answer was Blue"<br><br>);
+    counter+1;
+  if(counter = 0)
+    echo(" You answered " Math1 <br> "The correct answer was 6"<br><br>);
+    echo(" You answered " Math2 <br> "The correct answer was 5"<br><br>);
+    echo(" You answered " Math3 <br> "The correct answer was 8"<br><br>);
+    echo(" You answered " Math4 <br> "The correct answer was 18"<br><br>);
+    echo(" You answered " Color <br> "The correct answer was Blue"<br><br>);
+echo("You got " counter "out of 5 correct.")<br>;
+echo("You got " 100/counter "%");
+?>
